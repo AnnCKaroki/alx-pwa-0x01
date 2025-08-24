a2948d5770d57b7721fdca6605d0e9a35c94529e
Ann <annkarokiadmissions@gmail.com>
2025-08-24 18:22:08 +0300

    install update

--- alx-movie-app/postcss.config.mjs
+++ alx-movie-app/postcss.config.mjs
@@ -1,18 +1,5 @@
-import withPWAInit from "@ducanh2912/next-pwa";
-
-/** @type {import('next').NextConfig} */
-
-const withPWA = withPWAInit({
-  dest: 'public'
-})
-
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ['m.media-amazon.com'],
-  },
+const config = {
+  plugins: ["@tailwindcss/postcss"],
 };
 
-export default withPWA({
-  ...nextConfig
-})
+export default config;
